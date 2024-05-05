7de4dec9e4a8fdeb094de019460f2c3a7201f98e
amb10302 <amb10302@nyu.edu>
2024-05-05 15:01:34 -0400

    Add files via upload

--- arduincommcode.py
+++ arduincommcode.py
@@ -1,8 +1,10 @@
+from pynput import keyboard
+
 while(1):
-	with keyboard.Events() as events:
-	    # Block for as much as possible
-	    event = events.get(1e6)
-	    if event.key == keyboard.KeyCode.from_char('a'):
-	        print("s")
-	    elif event.key == keyboard.KeyCode.from_char('w'):
-	    	print("n")
+    with keyboard.Events() as events:
+        # Block for as much as possible
+        event = events.get(1e6)
+        if event.key == keyboard.KeyCode.from_char('a'):
+            print("s")
+        elif event.key == keyboard.KeyCode.from_char('w'):
+            print("n")
