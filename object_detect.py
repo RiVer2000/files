4e6d30564bf733e6c645cf1125fb78b51704c5f6
Rishabh Verma <64482413+RiVer2000@users.noreply.github.com>
2024-05-05 01:24:08 -0400

    view pred

--- object_detect.py
+++ object_detect.py
@@ -56,7 +56,7 @@ with torch.no_grad():
             last_logged = now
             frame_count = 0
 
-top = list(enumerate(output[0].softmax(dim=0)))
-top.sort(key=lambda x: x[1], reverse=True)
-for idx, val in top[:10]:
-    print(f"{val.item()*100:.2f}% {classes[idx]}")
+        top = list(enumerate(output[0].softmax(dim=0)))
+        top.sort(key=lambda x: x[1], reverse=True)
+        for idx, val in top[:10]:
+            print(f"{val.item()*100:.2f}% {classes[idx]}")
