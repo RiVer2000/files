f1da1c56a224bb56dca6d3a45d9adeb8a9a67d0d
amb10302 <amb10302@nyu.edu>
2024-05-05 14:22:50 -0400

    Add files via upload

---
+++ arduincommcode.py
@@ -0,0 +1,8 @@
+while(1):
+	with keyboard.Events() as events:
+	    # Block for as much as possible
+	    event = events.get(1e6)
+	    if event.key == keyboard.KeyCode.from_char('a'):
+	        print("s")
+	    elif event.key == keyboard.KeyCode.from_char('w'):
+	    	print("n")
