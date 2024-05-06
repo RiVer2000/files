dadb06e32e7fbbd32810cdb108be80514ecc512c
RiVer2000 <vermarishabh2000@gmail.com>
2024-05-06 17:33:46 -0400

    ultrasonic

--- pi2uno.py
+++ pi2uno.py
@@ -1,10 +1,20 @@
 import serial
 from time import sleep
 sleep(1)
-ser = serial.Serial('/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_85439303333351612151-if00',9600);
+ser = serial.Serial('/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_85439303333351612151-if00',115200);
+camflag = 0
+
+rxmsg = ser.readline()
+print(rxmsg)
 
 while 1:
 	charcommand = input("Type a letter:")
 	ser.write(charcommand.encode('utf-8'))
-
+	camflag = ser.readline()
+	#print(camflag)
+	sleep(0.5)
+	if (camflag.strip):
+		print("take pic and do stuff")
+		sleep(5)
+		camflag = 0
 
