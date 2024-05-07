e0be33e5edf500deb85d27fa43c8ea8b22a4296e
sanjos24 <167492234+sanjos24@users.noreply.github.com>
2024-05-07 16:42:52 -0400

    Update py_comm.py

--- py2cpp/py_comm.py
+++ py2cpp/py_comm.py
@@ -3,11 +3,7 @@ import sys
 if len(sys.argv) > 1:
     integer = int(sys.argv[1])  # Convert input to integer
 
-    # Print the received integer
-    print("Received integer:", integer)
-
-    # Check if the integer is divisible by 6
+    # Only send the integer back to C++ if it's divisible by 6
     if integer % 6 == 0:
-        # Send the integer back to cpp_comm.cpp using stdout
         sys.stdout.write(str(integer) + "\n")
         sys.stdout.flush()
