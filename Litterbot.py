ce22381fc14bc2de78651e941aa1acdaa7f422cb
Rishabh Verma <64482413+RiVer2000@users.noreply.github.com>
2024-05-08 19:39:52 -0400

    integrate

--- Litterbot.py
+++ Litterbot.py
@@ -1,5 +1,6 @@
 import tty, sys, termios, serial
 from time import sleep
+import object_detection
 
 ser = serial.Serial('/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_85439303333351612151-if00',115200,bytesize=8, parity='N', stopbits=1, timeout=None);
 
@@ -14,14 +15,14 @@ while 1:
   ser.write(x.encode('ascii'))
   print("You pressed", x)
   if (ser.inWaiting()>0):
-    camflag=ser.read(ser.inWaiting())
+    rx=ser.read(ser.inWaiting())
     #capture pic
-    #use tensor flow function
+    camflag = object_detection.trash_detect()
     ser.write('m'.encode('ascii')) #deploy arm
     sleep(3)
     ser.write('c'.encode('ascii')) #close gripper
 
-    camflag = 0
+    
     if (camflag == 1):
       ser.write('n'.encode('ascii')) #stow arm
       ser.write('o'.encode('ascii')) #open gripper
