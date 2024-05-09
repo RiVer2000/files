309ae81860d2a2ca586a5429be972d5447f837e5
Rishabh Verma <64482413+RiVer2000@users.noreply.github.com>
2024-05-08 21:24:28 -0400

    Update capture_headless.py

--- capture_headless.py
+++ capture_headless.py
@@ -2,16 +2,27 @@
 
 from picamera2 import Picamera2
 
-def capture():
+camera_busy = False  # Flag to indicate whether the camera is currently in use
 
-  picam2 = Picamera2()
-  config = picam2.create_still_configuration()
-  picam2.configure(config)
-  
-  picam2.start()
-  
-  # np_array = picam2.capture_array()
-  # print(np_array)
-  picam2.capture_file("trash.jpg")
-  picam2.stop()
-  
+def capture():
+    global camera_busy
+    
+    if camera_busy:
+        print("Camera is currently busy. Skipping capture.")
+        return
+    
+    camera_busy = True
+    
+    try:
+        picam2 = Picamera2()
+        config = picam2.create_still_configuration()
+        picam2.configure(config)
+
+        picam2.start()
+
+        # np_array = picam2.capture_array()
+        # print(np_array)
+        picam2.capture_file("trash.jpg")
+        picam2.stop()
+    finally:
+        camera_busy = False
